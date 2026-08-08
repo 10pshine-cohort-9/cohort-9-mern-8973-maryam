63a50f669de383cbcf4a148d2043bed0fb1fb416
Maryamhammid <maryamhamidmajeed@gmail.com>
2026-08-09 02:21:14 +0500

    feat: configure MongoDB Atlas connection with Mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,15 @@
 const app = require("./src/app");
 const logger = require("./src/config/logger");
+const connectDB = require("./src/config/db");
 
 const PORT = process.env.PORT || 5000;
+
+const startServer = async () => {
+  await connectDB();
 
 app.listen(PORT, () => {
   logger.info(`Server is running on port ${PORT}`);
 });
+};
+
+startServer();
